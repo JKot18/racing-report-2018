b872e6ce8e2e07239005bacbf886e2a5666802b5
Julia_Babik <j.babik.ctr@1010data.com>
2023-05-15 21:55:50 +0200

    report of the monaco race 2018, package

---
+++ -/src/_/__about__.py
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: 2023-present Julia_Kot <julia.kot.ent@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+__version__ = "0.0.1"
---
+++ -/src/_/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: 2023-present Julia_Kot <julia.kot.ent@gmail.com>
+#
+# SPDX-License-Identifier: MIT
